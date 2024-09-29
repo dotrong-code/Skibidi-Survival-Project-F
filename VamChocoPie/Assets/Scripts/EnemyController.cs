9f00d716762d33f1d9d31b1e87751e54f1630391
dotrong-code <dotrong159357@gmail.com>
2024-09-29 23:27:27 +0700

    done part 6

---
+++ VamChocoPie/Assets/Scripts/EnemyController.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyController : MonoBehaviour
+{
+    public Rigidbody2D theRB;
+    public float moveSpeed;
+    private Transform target;
+
+    public float damage;
+
+    public float hitWaitTime = 1f;
+    private float hitCounter;
+    // Start is called before the first frame update
+    void Start()
+    {
+        target = FindObjectOfType<PlayerController>().transform;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        theRB.velocity = (target.position - transform.position).normalized * moveSpeed;
+    
+        if (hitCounter > 0f)
+        {
+            hitCounter -= Time.deltaTime;
+        }
+    }
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if(collision.gameObject.tag == "Player" && hitCounter <= 0f)
+        {
+            PlayerHealthController.instance.TakeDamage(damage);
+
+            hitCounter = hitWaitTime;
+        }
+    }
+}
--- VamChocoPie/Assets/Scripts/PlayerController.cs
+++ VamChocoPie/Assets/Scripts/PlayerController.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     public float moveSpeed;
+    public Animator anim;
     // Start is called before the first frame update
     void Start()
     {
@@ -14,6 +15,21 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        Vector3 moveInput = new Vector3(0f,0f,0f);
+        moveInput.x = Input.GetAxisRaw("Horizontal");
+        moveInput.y = Input.GetAxisRaw("Vertical");
+
+        moveInput.Normalize();
+
+        transform.position += moveInput * moveSpeed * Time.deltaTime;
+
+        if(moveInput != Vector3.zero)
+        {
+            anim.SetBool("IsMoving", true);
+        }
+        else
+        {
+            anim.SetBool("IsMoving", false);
+        }
     }
 }
